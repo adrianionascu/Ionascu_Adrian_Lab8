15597e3586a6169e0d1ec32a8316ef976049d714
Ioan Adrian Ionascu <aionascu99@gmail.com>
2021-11-28 18:55:01 +0200

    Add project files.

---
+++ Data/Ionascu_Adrian_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ionascu_Adrian_Lab8.Models;
+
+namespace Ionascu_Adrian_Lab8.Data
+{
+    public class Ionascu_Adrian_Lab8Context : DbContext
+    {
+        public Ionascu_Adrian_Lab8Context (DbContextOptions<Ionascu_Adrian_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ionascu_Adrian_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Ionascu_Adrian_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ionascu_Adrian_Lab8.Data;
+using Ionascu_Adrian_Lab8.Models;
+
+namespace Ionascu_Adrian_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ionascu_Adrian_Lab8.Data.Ionascu_Adrian_Lab8Context _context;
+
+        public IndexModel(Ionascu_Adrian_Lab8.Data.Ionascu_Adrian_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+             Book = await _context.Book
+            .Include(b => b.Publisher)
+            .ToListAsync();
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
